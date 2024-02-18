7e651217eed0fb6e534321f4962a317ea172d605
ellenmariadev <ellenmariadev@gmail.com>
2024-02-18 03:39:55 -0300

    feat: add cors configuration

---
+++ src/main/java/com/example/oasismaritimo/infra/auth/CorsConfiguration.java
@@ -0,0 +1,21 @@
+package com.example.oasismaritimo.infra.auth;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.web.cors.CorsConfigurationSource;
+import org.springframework.web.cors.UrlBasedCorsConfigurationSource;
+
+import java.util.List;
+
+public class CorsConfiguration {
+    @Bean
+    public CorsConfigurationSource corsConfigurationSource() {
+        org.springframework.web.cors.CorsConfiguration configuration = new org.springframework.web.cors.CorsConfiguration();
+        configuration.setAllowedOrigins(List.of("*"));
+        configuration.setAllowedMethods(List.of("GET","POST", "PUT", "DELETE"));
+        configuration.setAllowCredentials(true);
+        configuration.setAllowedHeaders(List.of("Authorization", "Cache-Control", "Content-Type"));
+        UrlBasedCorsConfigurationSource source = new UrlBasedCorsConfigurationSource();
+        source.registerCorsConfiguration("/**", configuration);
+        return source;
+    }
+}
