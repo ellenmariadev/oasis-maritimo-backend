469b0231d05007e593c0d59562d7d348863f2529
ellenmariadev <ellenmariadev@gmail.com>
2024-02-20 21:33:06 -0300

    refactor: set database timezone UTC

--- src/main/java/com/example/oasismaritimo/domain/dto/animal/AnimalResponseDTO.java
+++ src/main/java/com/example/oasismaritimo/domain/dto/animal/AnimalResponseDTO.java
@@ -2,6 +2,7 @@ package com.example.oasismaritimo.domain.dto.animal;
 
 import com.example.oasismaritimo.domain.model.Animal;
 import com.example.oasismaritimo.domain.model.Specie;
+import com.fasterxml.jackson.annotation.JsonFormat;
 
 import java.util.Date;
 import java.util.UUID;
@@ -11,6 +12,7 @@ public record AnimalResponseDTO(
         String name,
         Integer age,
         Specie specie,
+        @JsonFormat(shape = JsonFormat.Shape.STRING, pattern = "yyyy-MM-dd")
         Date arrivalDate,
         String diet,
         Double weight,
